5cff1d0b87f131d0e29383279b6cb17ee153b2fb
Neus Aguera <nagueravalls@gmail.com>
2019-11-04 17:15:31 +0700

    fix close toggleMenu

--- themes/solid/assets/js/menu.js
+++ themes/solid/assets/js/menu.js
@@ -1,12 +1,21 @@
-function toggleMenu() {
-    var openMenu = document.getElementById("menu");
-    if (hasClass(openMenu, 'close')) {
-        openMenu.classList = "menu"
-    } else {
-        openMenu.classList = "menu close"
-    }
-}
+var openMenu = document.getElementById("menu");
+var btnMenu = document.getElementById("bars-menu");
 
 function hasClass(elem, className) {
     return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
 }
+
+document.addEventListener('click', function(event) {
+  var isClickInside = openMenu.contains(event.target);
+  var isClickBtn = btnMenu.contains(event.target);
+
+  if (isClickInside ){
+     openMenu.classList = "menu close";
+  } else {
+      if(isClickBtn){
+          openMenu.classList = "menu";
+      } else {
+          openMenu.classList = "menu close";
+      }
+  }
+});
